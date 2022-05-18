c6e523c2f46c41463202d14abdc8d48ac0386195
Pradeep Jadhav <42565481+Pradeep17Jadhav@users.noreply.github.com>
2022-05-18 22:05:21 +0530

    reduce board height

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
-	const [chars, setChars] = useState("abcdefghijklmnopqrstuvwxyz");
+	const mainString = useRef("abcdefghijklmnopqrstuvwxyz");
+	// setChars("abcdefghijklmnopqrstuvwxyz");
+
+	const [chars, setChars] = useState(mainString.current);
+	const [running, setRunning] = useState(false);
 	const [currChar, setCurrChar] = useState("");
 	const [inputValue, setInputValue] = useState("");
 	const [startTime, setStartTime] = useState(0);
 	const [endTime, setEndTime] = useState(0);
-	let finishMessage = null;
+	const [timeTaken, setTimeTaken] = useState(0);
+	const [intervalId, setIntervalId] = useState(false);
+	const [bWon, setWon] = useState(false);
 
-	const GetLetter = () => {
+
+	const getLetter = () => {
 		console.log(chars, chars.length);
 		let newCurrChar = "";
 		if(chars.length) {
@@ -19,28 +25,55 @@ function App() {
 			setChars(chars.slice(0, index) + chars.slice(index + 1));
 		}
 		setCurrChar(newCurrChar);
-
 		return newCurrChar;
 	}
 
 	useEffect(() => {
-		GetLetter();
+		getLetter();
 	}, []);
 
-	useEffect(()=>{
-		console.log("Ended at ", startTime);
-	}, [endTime]);
+	useEffect(() => {
+		if(chars == mainString.current)
+			getLetter();
+	}, [chars]);
 
-	const CheckInput = (evt) => {
-		if(evt.nativeEvent.data == currChar || evt.nativeEvent.data == inputValue + currChar) {
-			if(startTime == 0) {
-				setStartTime(new Date().getTime());
-			}
+	const startGame = () => {
+		let time = new Date().getTime();
+		setStartTime(time);
+		setRunning(true);
+
+		if(intervalId) {
+			clearInterval(intervalId);
+		}
+
+		const id = setInterval(() => {
+			setTimeTaken(new Date().getTime() - time);
+		}, 1);
+		setIntervalId(id);
+	}
+
+	const endGame = () => {
+		setEndTime(new Date().getTime());
+		setCurrChar("Done!");
+		clearTimer();
+	}
+
+	const clearTimer = () => {
+		if(intervalId) {
+			clearInterval(intervalId);
+		}
+		setIntervalId(0);
+	}
+
+	const checkInput = (evt) => {
+		if(startTime != 0 && evt.nativeEvent.data == currChar || evt.nativeEvent.data == inputValue + currChar) {
+			if(!running)
+				startGame();
 
 			let char = currChar;
-			let newCurrChar = GetLetter();
+			let newCurrChar = getLetter();
 			if(newCurrChar == "") {
-				setEndTime(new Date().getTime());
+				endGame();
 			}
 			setInputValue(inputValue + char);
 		}
@@ -51,15 +84,46 @@ function App() {
 		return false;
     };
 
+	const onReset = () => {
+		clearTimer();
+		setStartTime(0);
+		setEndTime(0);
+		setRunning(false);
+		setTimeTaken(0);
+		setWon(false);
+		setInputValue("");
+		setChars(mainString.current);
+		let inputbox = document.getElementById("inputbox");
+		if(inputbox)
+			inputbox.focus();
+	}
+
 	return (
 		<div className="App">
-			<h1>Typing Speed Challenge</h1>
+			<h1 id="title">Typing Speed Challenge</h1>
+			<p id="tagline">Let's see how fast you can type the random alphabets!</p>
 			<div id="board">
 				{currChar}
 			</div>
-			<input type="text" id="inputbox" value={inputValue} onInput={evt => CheckInput(evt)} onPaste={evt => onTextCopyPaste(evt)} onCopy={evt => onTextCopyPaste(evt)}></input>
-			<button id="reset">Reset</button>
-			{endTime != 0 ? <div>You took {endTime - startTime} milliseconds to complete.</div> : null}
+			<input 
+				type="text" 
+				id="inputbox" 
+				value={inputValue} 
+				placeholder="Type here to start..."
+				onInput={evt => checkInput(evt)} 
+				onPaste={evt => onTextCopyPaste(evt)} 
+				onCopy={evt => onTextCopyPaste(evt)}>
+			</input>
+
+			<button 
+				id="btnreset"
+				onClick={evt => onReset(evt)}
+			>
+				Reset
+			</button>
+
+			<p id="timer">Time: {(timeTaken / 1000).toFixed(3)} s</p>
+			<p id="credit">Created by <a href="https://www.linkedin.com/in/pradeep-jadhav/" target="_blank"><b>Pradeep Jadhav</b></a></p>
 		</div>
 	);
 }
